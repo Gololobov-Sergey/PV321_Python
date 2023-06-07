2d4ad67b221534d0b6b2a798ef355f2e54fc35d5
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-06-07 20:43:29 +0300

    Добавьте файлы проекта.

---
+++ PV321_Python/Lesson 1.py
@@ -0,0 +1,53 @@
+﻿'''print("Hello", "Python", 123, end=' ', sep=' -=- ')
+print("Hello", "Python", 123)
+print(2+3) # це вираз
+print("Hello 'Python'") 
+print('Hello "Python"')
+print("Hello \"Python\"")
+print("\nHello")
+print("\tHello?")
+print("\t\tHello")
+print("\a")
+print("Слава Україні!")
+#print("\\/\\/\\/\\/\\/\\")
+
+print(1_111_111_111)
+print(-1_111_111_111)
+print(0o27)
+print(0xFF)
+print(0xff)
+print(0b10111)
+print(ord('a'))
+
+print(12.45)
+print(12.22222287987)
+print(12.)
+print(0.00000000000000000000000000001)
+print(10.2E-29)
+
+print(True)
+print(False)
+
+# int
+# float
+# str
+# bool
+
+# A-Z, a-z, 0-9, _
+
+# __1a
+
+b = 9
+a = 1
+a = "mama"'''
+
+#sumGrn = 5
+
+#print("Summa =", sumGrn)
+
+# task 1
+a = float(input("Введить значення сторони а: "))
+b = float(input("Введить значення сторони b: "))
+s = a * b
+print(f"Площа прямокутника зі сторонами а = {a}, b = {b} дорівнює {s} кв.од")
+
